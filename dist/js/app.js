33e5864eed029073a3bc72f20aecfd88aa0cea03
Dávid Török <torok.david@diak.remenyikzs.hu>
2024-09-26 18:16:43 +0200

    update

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,29 +1,43 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
     const start_buttonEl = document.getElementById('start_button');
+    const allButtonElUnFiltered = Array.from(document.querySelectorAll('button'));
+    const allButtonEl = Array.from(allButtonElUnFiltered).filter(button => { var _a; return ((_a = button.textContent) === null || _a === void 0 ? void 0 : _a.trim()) !== "Start"; });
     const svgImagesList = ['airplane', 'alarmFill', 'backpack', 'ballon', 'binoculars', 'earth', 'house', 'sun'];
     const buttonElementsList = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'threeteen', 'fourteen', 'fiveteen', 'sixteen'];
+    let gemLogic;
     if (start_buttonEl) {
         start_buttonEl.addEventListener('click', () => {
             console.log('Clicked');
-            const gemLogic = new GemLogic(buttonElementsList, svgImagesList);
+            gemLogic = new GemLogic(buttonElementsList, svgImagesList);
             gemLogic.addImageToBtn();
+            allButtonEl.forEach(btn => {
+                gemLogic.forceTheMemorize(btn);
+            });
+        });
+    }
+    if (allButtonEl) {
+        allButtonEl.forEach(buttom => {
+            buttom.addEventListener('click', () => {
+                gemLogic.getButtomImage(buttom, allButtonEl);
+                gemLogic.removeButtomFromGameArea(buttom);
+            });
         });
     }
 });
 class GemLogic {
     constructor(btnElL, svgImgL) {
+        this.inGameImageList = [];
         this.buttonElementsList = btnElL;
         this.svgImagesList = svgImgL;
     }
     addImageToBtn() {
-        for (let i = 0; i < 16; i++) {
-            const chosenSvg = this.svgImagesList[Math.floor(Math.random() * this.buttonElementsList.length)];
-            const indexSvgL = this.svgImagesList.indexOf(chosenSvg);
-            this.svgImagesList.splice(indexSvgL, 1);
+        for (let i = 0; i < 8; i++) {
+            const chosenSvg = this.svgImagesList[Math.floor(Math.random() * this.svgImagesList.length)];
             for (let j = 0; j < 2; j++) {
                 const imageEl = document.createElement('img');
                 imageEl.src = `../svgs/${chosenSvg}.svg`;
+                imageEl.alt = chosenSvg;
                 const chosenButton = this.buttonElementsList[Math.floor(Math.random() * this.buttonElementsList.length)];
                 const chosenButtonEl = document.getElementById(chosenButton);
                 const indexBtnL = this.buttonElementsList.indexOf(chosenButton);
@@ -32,6 +46,56 @@ class GemLogic {
                     chosenButtonEl.insertBefore(imageEl, chosenButtonEl.firstChild);
                 }
             }
+            const indexSvgL = this.svgImagesList.indexOf(chosenSvg);
+            this.svgImagesList.splice(indexSvgL, 1);
         }
+    }
+    getButtomImage(buttom, allButtonEl) {
+        const image = buttom.querySelector('img');
+        if (image && image.src) {
+            const imgSRc = image.src;
+            this.inGameImageList.push(imgSRc.toString());
+            this.inGameImageList.forEach(i => console.log('content of the list: ' + i));
+            console.log(this.inGameImageList.length);
+            this.compareTwoElemtOfList(this.inGameImageList, buttom, allButtonEl);
+        }
+        else {
+            console.log('No image found');
+        }
+    }
+    compareTwoElemtOfList(inGameImageList, buttom, allButtonEl) {
+        let setTimeOutAnonim = function () {
+            setTimeout(() => {
+                console.clear();
+            }, 2000);
+        };
+        if (inGameImageList.length >= 2) {
+            if (inGameImageList[0] === inGameImageList[1]) {
+                console.log('Tahats rihgt');
+                this.inGameImageList = [];
+                setTimeOutAnonim();
+            }
+            else {
+                console.log('No match');
+                setTimeOutAnonim();
+            }
+        }
+    }
+    removeButtomFromGameArea(buttom) {
+        const image = buttom.querySelector('img');
+        if (image) {
+            image.remove();
+        }
+        buttom.disabled = true;
+        buttom.style.backgroundColor = 'rgb(000, 000, 000)';
+    }
+    forceTheMemorize(buttom) {
+        setTimeout(() => {
+            buttom.style.background = 'rgb(256, 256, 256)';
+            const imgeElL = buttom.querySelector('img');
+            if (imgeElL) {
+                imgeElL.style.display = 'none';
+            }
+        }, 8000);
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,35 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     const start_buttonEl: HTMLButtonElement = document.getElementById('start_button')! as HTMLButtonElement;
+    const allButtonElUnFiltered: HTMLButtonElement[] = Array.from(document.querySelectorAll('button')) as HTMLButtonElement[];
+    const allButtonEl: HTMLButtonElement[] = Array.from(allButtonElUnFiltered).filter(button => button.textContent?.trim() !== "Start");
 
     const svgImagesList: string[] = ['airplane', 'alarmFill', 'backpack', 'ballon', 'binoculars', 'earth', 'house', 'sun'];
     const buttonElementsList: string[] = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'threeteen', 'fourteen', 'fiveteen', 'sixteen'];
 
+
+    let gemLogic: GemLogic;
     if (start_buttonEl) 
     {
         start_buttonEl.addEventListener('click', () => {
             console.log('Clicked');
-            const gemLogic: GemLogic = new GemLogic(buttonElementsList, svgImagesList);
+
+            gemLogic = new GemLogic(buttonElementsList, svgImagesList);
             gemLogic.addImageToBtn();
+
+            allButtonEl.forEach(btn => {
+                gemLogic.forceTheMemorize(btn);
+            });
+        });
+    }
+
+
+    if(allButtonEl)
+    {
+        allButtonEl.forEach(buttom => {
+            buttom.addEventListener('click', () => {
+                gemLogic.getButtomImage(buttom, allButtonEl);
+                gemLogic.removeButtomFromGameArea(buttom);
+            });
         });
     }
 });
@@ -19,6 +39,7 @@ class GemLogic
 {
     private buttonElementsList: string[];
     private svgImagesList: string[];
+    private inGameImageList: string[] = [];
 
     constructor(btnElL: string[], svgImgL: string[]) 
     {
@@ -28,21 +49,17 @@ class GemLogic
 
     public addImageToBtn()
     {
-        //
-        for(let i = 0; i < 16; i++)
+        for(let i = 0; i < 8; i++)
         {
             //svg imge
-            const chosenSvg: string = this.svgImagesList[Math.floor(Math.random() * this.buttonElementsList.length)]; //chose a roandom svg image
-            
-            const indexSvgL: number = this.svgImagesList.indexOf(chosenSvg); //finde the chosen svg image index
-            this.svgImagesList.splice(indexSvgL, 1); //remove the chosen indexed elemt from the basic list
+            const chosenSvg: string = this.svgImagesList[Math.floor(Math.random() * this.svgImagesList.length)]; //chose a roandom svg image
             
             //add image to buttom
-            
             for (let j = 0; j < 2; j++) //2 range for loop to add the same image to diferent buttoms
             {
                 const imageEl: HTMLImageElement = document.createElement('img')! as HTMLImageElement; //create image elemt
                 imageEl.src = `../svgs/${chosenSvg}.svg`; //add chosen svg image to the img elemt
+                imageEl.alt = chosenSvg;
                 //btn elemt
                 const chosenButton: string = this.buttonElementsList[Math.floor(Math.random() * this.buttonElementsList.length)]; //find the chosen button
                 const chosenButtonEl: HTMLButtonElement = document.getElementById(chosenButton)! as HTMLButtonElement; //get the chosen buttom elemt 
@@ -55,6 +72,77 @@ class GemLogic
                     chosenButtonEl.insertBefore(imageEl, chosenButtonEl.firstChild); // add the created image to buttom
                 }
             }
+
+            const indexSvgL: number = this.svgImagesList.indexOf(chosenSvg); //finde the chosen svg image index
+            this.svgImagesList.splice(indexSvgL, 1); //remove the chosen indexed elemt from the basic list
         }
+    }
+
+    public getButtomImage(buttom: HTMLButtonElement, allButtonEl: HTMLButtonElement[])
+    {
+        const image: HTMLImageElement = buttom.querySelector('img')! as  HTMLImageElement;
+
+        if(image && image.src)
+        {
+            const imgSRc = image.src;
+            this.inGameImageList.push(imgSRc.toString());
+            this.inGameImageList.forEach(i => console.log('content of the list: ' + i));
+            console.log(this.inGameImageList.length);
+            this.compareTwoElemtOfList(this.inGameImageList, buttom, allButtonEl);
+        }
+        else
+        {
+            console.log('No image found');
+        }
+    }
+
+    private compareTwoElemtOfList(inGameImageList: string[], buttom: HTMLButtonElement, allButtonEl: HTMLButtonElement[])
+    {
+
+        let setTimeOutAnonim: () => void = function() {
+            setTimeout(() => {
+                console.clear();
+            }, 2000);
+        };
+
+        if(inGameImageList.length >= 2)
+        {
+            if(inGameImageList[0] === inGameImageList[1])
+            {
+                console.log('Tahats rihgt')
+                this.inGameImageList = [];
+
+                setTimeOutAnonim();
+            }
+            else
+            {
+                console.log('No match');
+                setTimeOutAnonim();
+            }
+        }
+    }
+
+    public removeButtomFromGameArea(buttom: HTMLButtonElement)
+    {
+        const image = buttom.querySelector('img');
+        if(image)
+        {
+            image.remove();
+        }
+        buttom.disabled = true;
+        buttom.style.backgroundColor = 'rgb(000, 000, 000)';
+    }
+
+    public forceTheMemorize(buttom: HTMLButtonElement)
+    {
+        setTimeout(() => {
+            buttom.style.background = 'rgb(256, 256, 256)'; 
+            const imgeElL: HTMLImageElement = buttom.querySelector('img') as HTMLImageElement;
+            if(imgeElL)
+            {
+                imgeElL.style.display = 'none';
+            }
+        }, 8000)
+        
     }
 }
